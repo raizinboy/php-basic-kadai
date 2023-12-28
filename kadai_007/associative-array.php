63a56a32685de349be337dc9b473f89274cef665
inada-ryuto <nyankoman1715@gmail.com>
2023-12-28 22:27:18 +0900

    kadai_007の追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset='UTF-8'>
+        <title>kadai_007</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $item_info = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            
+            print_r($item_info);
+            ?>
+        </p>
+    </body>
+</html>
+

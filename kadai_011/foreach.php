5a87ada260c3b736e46dfd871eb2132b705b5a42
inada-ryuto <nyankoman1715@gmail.com>
2023-12-29 10:11:09 +0900

    kadai_011の追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset='UTF-8'>
+        <title>PHP基礎編</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            //商品の連想配列を作成
+            $item_info = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+            //連想配列の要素を一つずつ出力する
+            foreach ($item_info as $key => $value){
+                echo "{$key}:{$value}<br>";
+            }
+            ?>
+        </p>
+    </body>
+</html>
+
